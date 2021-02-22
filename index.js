c65c6f77b5c8366bf718521a15285ae328e7fb32
Momo707577045 <707577045@qq.com>
2021-02-22 19:39:18 +0800

    # 添加遍历轮询

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ let groupIndex = (fileText.match(/(\d+).png/)[1] - 1) / 3
 console.log(groupIndex)
 
 groupIndex++
+groupIndex = groupIndex % 48
+
 fileText = fileText.replace(/(\d+).png/ig, function(...params) {
   return `${groupIndex * 3 + (params[1] - 1) % 3 + 1 }.png`
 })
